bc4893ef551c3458681956640892c734bc51a223
Jarno <jarno@limetric.com>
2018-06-21 19:19:16 +0200

    Misc download page work

--- routes/downloadsPage.js
+++ routes/downloadsPage.js
@@ -2,7 +2,14 @@ const Express = require('express');
 const router = Express.Router();
 
 router.get('/', (req, res, next) => {
-
+    const template = require('../views/downloadsIndex.marko');
+    res.marko(template, {
+        page: {
+            title: 'Downloads',
+            description: 'Downloads for the open-source adaption of RollerCoaster Tycoon 2. Free to download.',
+            path: App.getExpressPath(req.baseUrl, req.path)
+        },
+    });
 });
 
 module.exports = router;
