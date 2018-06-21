d593e9d79d7e00a0b8033b55664d5ca3eabf655d
Jarno <jarno@limetric.com>
2018-06-21 15:14:40 +0200

    Typo

--- routes/staticPages.js
+++ routes/staticPages.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+const Express = require('express');
+const router = Express.Router();
 
 router.get('/', (req, res, next) => {
     const template = require('../views/index.marko');
