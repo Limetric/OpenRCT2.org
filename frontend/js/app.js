879ecf3e6df4c614a2db21165977bc940d281a8a
Jarno <jarno@limetric.com>
2018-06-22 12:23:26 +0200

    Frontend JS moment update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,9 +1,13 @@
 //Babel Polyfill must always be first
 require('babel-polyfill');
 
+//Display dates and fromNow dates
 const moment = require('moment');
 for (const el of document.querySelectorAll('.date')) {
-    el.textContent = moment(new Date(el.dataset.date || el.textContent)).format(el.dataset.format);
+    const parentheses = el.classList.contains('parentheses');
+    el.textContent = (parentheses ? '(' : '') + moment(new Date(el.dataset.date || el.textContent)).format(el.dataset.format) + (parentheses ? ')' : '');
 }
-
-console.log('JS loaded');
+for (const el of document.querySelectorAll('.fromNow')) {
+    const parentheses = el.classList.contains('parentheses');
+    el.textContent = (parentheses ? '(' : '') + moment(new Date(el.dataset.date || el.textContent)).fromNow() + (parentheses ? ')' : '');
+}
